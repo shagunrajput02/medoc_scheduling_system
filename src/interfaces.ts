ad4af67637461c65879feb4cfb842d04909c8311
Shagun Chauhan <87222048+shagunrajput02@users.noreply.github.com>
2023-09-30 00:43:42 +0530

    Update interfaces.ts

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,1 +1,41 @@
+// Export and define interfaces to represent different entities
+export interface Person {
+  id: number;
+  name: string;
+}
 
+export interface Doctor extends Person {
+  workingHours: { start: string; end: string };
+  availableDays: string[];
+}
+
+export interface Patient extends Person {}
+
+export interface Appointment {
+  id: number;
+  patientId: number;
+  doctorId: number;
+  dateTime: string;
+}
+
+export interface Surgery {
+  id: number;
+  patientId: number;
+  doctorId: number;
+  operationTheatreId: number;
+  dateTime: string;
+  details: string;
+}
+
+export interface Task {
+  id: number;
+  description: string;
+  dueDate: string;
+  doctorId: number;
+}
+
+export interface OperationTheatre {
+  id: number;
+  name: string;
+  availableSlots: string[];
+}
