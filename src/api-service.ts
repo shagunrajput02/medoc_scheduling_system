115e763ab2adbfc7ad11cdccd8ec831fb6821e40
Shagun Chauhan <87222048+shagunrajput02@users.noreply.github.com>
2023-09-30 00:53:50 +0530

    Update api-service.ts

--- src/api-service.ts
+++ src/api-service.ts
@@ -1,24 +1,101 @@
+// api-service.ts
+
+import { Doctor, Patient, Appointment, Surgery, Task, OperationTheatre } from './interfaces';
+
 class ApiService {
+  // Example API endpoints
+  private doctorsApiUrl = '/api/doctors';
+  private patientsApiUrl = '/api/patients';
+  private appointmentsApiUrl = '/api/appointments';
+  private surgeriesApiUrl = '/api/surgeries';
+  private tasksApiUrl = '/api/tasks';
+  private operationTheatresApiUrl = '/api/operation-theatres';
+
   async fetchDoctors(): Promise<Doctor[]> {
-    // Example: Fetch doctors from an API endpoint
-    const response = await fetch('/api/doctors');
-    if (!response.ok) {
-      throw new Error('Failed to fetch doctors.');
+    try {
+      const response = await fetch(this.doctorsApiUrl);
+      if (!response.ok) {
+        throw new Error('Failed to fetch doctors.');
+      }
+      const data = await response.json();
+      return data as Doctor[];
+    } catch (error) {
+      console.error('Error fetching doctors:', error);
+      throw error;
     }
-    const data = await response.json();
-    return data;
   }
 
   async fetchPatients(): Promise<Patient[]> {
-    // Example: Fetch patients from an API endpoint
-    const response = await fetch('/api/patients');
-    if (!response.ok) {
-      throw new Error('Failed to fetch patients.');
+    try {
+      const response = await fetch(this.patientsApiUrl);
+      if (!response.ok) {
+        throw new Error('Failed to fetch patients.');
+      }
+      const data = await response.json();
+      return data as Patient[];
+    } catch (error) {
+      console.error('Error fetching patients:', error);
+      throw error;
     }
-    const data = await response.json();
-    return data;
   }
 
-  // Implement similar functions for other entities
+  async fetchAppointments(): Promise<Appointment[]> {
+    try {
+      const response = await fetch(this.appointmentsApiUrl);
+      if (!response.ok) {
+        throw new Error('Failed to fetch appointments.');
+      }
+      const data = await response.json();
+      return data as Appointment[];
+    } catch (error) {
+      console.error('Error fetching appointments:', error);
+      throw error;
+    }
+  }
+
+  async fetchSurgeries(): Promise<Surgery[]> {
+    try {
+      const response = await fetch(this.surgeriesApiUrl);
+      if (!response.ok) {
+        throw new Error('Failed to fetch surgeries.');
+      }
+      const data = await response.json();
+      return data as Surgery[];
+    } catch (error) {
+      console.error('Error fetching surgeries:', error);
+      throw error;
+    }
+  }
+
+  async fetchTasks(): Promise<Task[]> {
+    try {
+      const response = await fetch(this.tasksApiUrl);
+      if (!response.ok) {
+        throw new Error('Failed to fetch tasks.');
+      }
+      const data = await response.json();
+      return data as Task[];
+    } catch (error) {
+      console.error('Error fetching tasks:', error);
+      throw error;
+    }
+  }
+
+  async fetchOperationTheatres(): Promise<OperationTheatre[]> {
+    try {
+      const response = await fetch(this.operationTheatresApiUrl);
+      if (!response.ok) {
+        throw new Error('Failed to fetch operation theatres.');
+      }
+      const data = await response.json();
+      return data as OperationTheatre[];
+    } catch (error) {
+      console.error('Error fetching operation theatres:', error);
+      throw error;
+    }
+  }
 }
+
+export default ApiService;
+
 
