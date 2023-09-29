1fb63eb5c2ba524ebfc0fd4aebcf38f13cb3a791
Shagun Chauhan <87222048+shagunrajput02@users.noreply.github.com>
2023-09-30 00:41:09 +0530

    Update api-service.ts

--- src/api-service.ts
+++ src/api-service.ts
@@ -1,1 +1,24 @@
+class ApiService {
+  async fetchDoctors(): Promise<Doctor[]> {
+    // Example: Fetch doctors from an API endpoint
+    const response = await fetch('/api/doctors');
+    if (!response.ok) {
+      throw new Error('Failed to fetch doctors.');
+    }
+    const data = await response.json();
+    return data;
+  }
+
+  async fetchPatients(): Promise<Patient[]> {
+    // Example: Fetch patients from an API endpoint
+    const response = await fetch('/api/patients');
+    if (!response.ok) {
+      throw new Error('Failed to fetch patients.');
+    }
+    const data = await response.json();
+    return data;
+  }
+
+  // Implement similar functions for other entities
+}
 
