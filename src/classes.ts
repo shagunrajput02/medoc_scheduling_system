baee575924af6dda2e734ca81f6ba1b90b50eb04
Shagun Chauhan <87222048+shagunrajput02@users.noreply.github.com>
2023-09-30 00:51:45 +0530

    Update classes.ts

--- src/classes.ts
+++ src/classes.ts
@@ -1,37 +1,70 @@
+// classes.ts
+import { Doctor, Patient, Appointment, Surgery, Task, OperationTheatre } from './interfaces';
+
 class HospitalScheduler {
-  private apiService: ApiService;
   private doctors: Doctor[] = [];
   private patients: Patient[] = [];
-  // Initialize other data entities similarly
-
-  constructor(apiService: ApiService) {
-    this.apiService = apiService;
-  }
-
-  async initializeDataFromAPI() {
-    this.doctors = await this.apiService.fetchDoctors();
-    this.patients = await this.apiService.fetchPatients();
-    // Initialize other data entities from the API
-  }
+  private appointments: Appointment[] = [];
+  private surgeries: Surgery[] = [];
+  private tasks: Task[] = [];
+  private operationTheatres: OperationTheatre[] = [];
 
   // Doctor Registration
-  registerDoctor(doctorData: Doctor) {
-    // Check if the doctor already exists
-    const existingDoctor = this.doctors.find((doc) => doc.id === doctorData.id);
-    if (!existingDoctor) {
-      this.doctors.push(doctorData);
-    }
+  registerDoctor(doctor: Doctor) {
+    this.doctors.push(doctor);
   }
 
   // Patient Registration
-  registerPatient(patientData: Patient) {
-    // Check if the patient already exists
-    const existingPatient = this.patients.find((patient) => patient.id === patientData.id);
-    if (!existingPatient) {
-      this.patients.push(patientData);
+  registerPatient(patient: Patient) {
+    this.patients.push(patient);
+  }
+
+  // Schedule Appointment
+  scheduleAppointment(appointment: Appointment) {
+    this.appointments.push(appointment);
+  }
+
+  // Schedule Surgery
+  scheduleSurgery(surgery: Surgery) {
+    this.surgeries.push(surgery);
+  }
+
+  // Assign Task
+  assignTask(task: Task) {
+    this.tasks.push(task);
+  }
+
+  // Define Doctor Availability
+  setDoctorAvailability(doctorId: number, workingHours: { start: string; end: string }, availableDays: string[]) {
+    const doctor = this.doctors.find((doc) => doc.id === doctorId);
+    if (doctor) {
+      doctor.workingHours = workingHours;
+      doctor.availableDays = availableDays;
     }
   }
 
-  // Modify other methods to work with API data
+  // Define Operation Theatre Availability
+  setOperationTheatreAvailability(operationTheatreId: number, availableSlots: string[]) {
+    const operationTheatre = this.operationTheatres.find((theatre) => theatre.id === operationTheatreId);
+    if (operationTheatre) {
+      operationTheatre.availableSlots = availableSlots;
+    }
+  }
+
+  // Display Scheduled Appointments
+  displayAppointments() {
+    this.appointments.forEach((appointment) => {
+      const patient = this.patients.find((p) => p.id === appointment.patientId);
+      const doctor = this.doctors.find((d) => d.id === appointment.doctorId);
+      console.log(
+        `Appointment ID: ${appointment.id}, Patient: ${patient?.name}, Doctor: ${doctor?.name}, Date and Time: ${appointment.dateTime}`
+      );
+    });
+  }
+
+  // Other display methods for surgeries, tasks, etc.
 }
+
+export default HospitalScheduler;
+
 
