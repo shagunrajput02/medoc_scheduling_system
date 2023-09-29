b83e901f0b6ad83b6c9f1d4a6cd898c0edc0064d
Shagun Chauhan <87222048+shagunrajput02@users.noreply.github.com>
2023-09-30 00:40:13 +0530

    Update classes.ts

--- src/classes.ts
+++ src/classes.ts
@@ -1,1 +1,37 @@
+class HospitalScheduler {
+  private apiService: ApiService;
+  private doctors: Doctor[] = [];
+  private patients: Patient[] = [];
+  // Initialize other data entities similarly
+
+  constructor(apiService: ApiService) {
+    this.apiService = apiService;
+  }
+
+  async initializeDataFromAPI() {
+    this.doctors = await this.apiService.fetchDoctors();
+    this.patients = await this.apiService.fetchPatients();
+    // Initialize other data entities from the API
+  }
+
+  // Doctor Registration
+  registerDoctor(doctorData: Doctor) {
+    // Check if the doctor already exists
+    const existingDoctor = this.doctors.find((doc) => doc.id === doctorData.id);
+    if (!existingDoctor) {
+      this.doctors.push(doctorData);
+    }
+  }
+
+  // Patient Registration
+  registerPatient(patientData: Patient) {
+    // Check if the patient already exists
+    const existingPatient = this.patients.find((patient) => patient.id === patientData.id);
+    if (!existingPatient) {
+      this.patients.push(patientData);
+    }
+  }
+
+  // Modify other methods to work with API data
+}
 
