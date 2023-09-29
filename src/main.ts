caf6baae658f435b809ed6cbbe0a689c3af147eb
Shagun Chauhan <87222048+shagunrajput02@users.noreply.github.com>
2023-09-30 00:57:50 +0530

    Update main.ts
    In this code:
    
    Instances of ApiService and HospitalScheduler are created.
    
    The initializeData function fetches data for doctors, patients, appointments, surgeries, tasks, and operation theatres from the API using the ApiService methods.
    
    The fetched data is used to register doctors and patients, schedule appointments and surgeries, assign tasks, and set availability.
    
    Finally, the initializeData function displays scheduled appointments, and you can add additional application logic or operations as needed.
    
    //need to ensure that the API endpoints and data structures match with the actual API's structure and requirements. Additionally, handle errors and edge cases as per the application's needs.

--- src/main.ts
+++ src/main.ts
@@ -1,143 +1,52 @@
-// Define interfaces to represent different entities
-interface Person {
-  id: number;
-  name: string;
-}
+// main.ts
+import HospitalScheduler from './classes';
+import ApiService from './api-service';
+import { Doctor, Patient, Appointment, Surgery, Task, OperationTheatre } from './interfaces';
 
-interface Doctor extends Person {
-  workingHours: { start: string; end: string };
-  availableDays: string[];
-}
-
-interface Patient extends Person {}
-
-interface Appointment {
-  id: number;
-  patientId: number;
-  doctorId: number;
-  dateTime: string;
-}
-
-interface Surgery {
-  id: number;
-  patientId: number;
-  doctorId: number;
-  operationTheatreId: number;
-  dateTime: string;
-  details: string;
-}
-
-interface Task {
-  id: number;
-  description: string;
-  dueDate: string;
-  doctorId: number;
-}
-
-interface OperationTheatre {
-  id: number;
-  name: string;
-  availableSlots: string[];
-}
-
-class HospitalScheduler {
-  private doctors: Doctor[] = [];
-  private patients: Patient[] = [];
-  private appointments: Appointment[] = [];
-  private surgeries: Surgery[] = [];
-  private tasks: Task[] = [];
-  private operationTheatres: OperationTheatre[] = [];
-
-  // Doctor Registration
-  registerDoctor(doctor: Doctor) {
-    this.doctors.push(doctor);
-  }
-
-  // Patient Registration
-  registerPatient(patient: Patient) {
-    this.patients.push(patient);
-  }
-
-  // Schedule Appointment
-  scheduleAppointment(appointment: Appointment) {
-    this.appointments.push(appointment);
-  }
-
-  // Schedule Surgery
-  scheduleSurgery(surgery: Surgery) {
-    this.surgeries.push(surgery);
-  }
-
-  // Assign Task
-  assignTask(task: Task) {
-    this.tasks.push(task);
-  }
-
-  // Define Doctor Availability
-  setDoctorAvailability(doctorId: number, workingHours: { start: string; end: string }, availableDays: string[]) {
-    const doctor = this.doctors.find((doc) => doc.id === doctorId);
-    if (doctor) {
-      doctor.workingHours = workingHours;
-      doctor.availableDays = availableDays;
-    }
-  }
-
-  // Define Operation Theatre Availability
-  setOperationTheatreAvailability(operationTheatreId: number, availableSlots: string[]) {
-    const operationTheatre = this.operationTheatres.find((theatre) => theatre.id === operationTheatreId);
-    if (operationTheatre) {
-      operationTheatre.availableSlots = availableSlots;
-    }
-  }
-
-  // Display Scheduled Appointments
-  displayAppointments() {
-    this.appointments.forEach((appointment) => {
-      const patient = this.patients.find((p) => p.id === appointment.patientId);
-      const doctor = this.doctors.find((d) => d.id === appointment.doctorId);
-      console.log(
-        `Appointment ID: ${appointment.id}, Patient: ${patient?.name}, Doctor: ${doctor?.name}, Date and Time: ${appointment.dateTime}`
-      );
-    });
-  }
-
-  // Other display methods for surgeries, tasks, etc.
-}
-
-// Example Usage
-
+// Create instances of ApiService and HospitalScheduler
+const apiService = new ApiService();
 const scheduler = new HospitalScheduler();
 
-// Register doctors
-scheduler.registerDoctor({
-  id: 1,
-  name: 'Dr. Alia',
-  workingHours: { start: '09:00', end: '17:00' },
-  availableDays: ['Monday', 'Wednesday', 'Friday'],
+// Initialize data from the API
+async function initializeData() {
+  try {
+    // Fetch data for doctors, patients, appointments, surgeries, tasks, and operation theatres
+    const doctors: Doctor[] = await apiService.fetchDoctors();
+    const patients: Patient[] = await apiService.fetchPatients();
+    const appointments: Appointment[] = await apiService.fetchAppointments();
+    const surgeries: Surgery[] = await apiService.fetchSurgeries();
+    const tasks: Task[] = await apiService.fetchTasks();
+    const operationTheatres: OperationTheatre[] = await apiService.fetchOperationTheatres();
+
+    // Register doctors and patients
+    doctors.forEach((doctor) => scheduler.registerDoctor(doctor));
+    patients.forEach((patient) => scheduler.registerPatient(patient));
+
+    // Schedule appointments and surgeries
+    appointments.forEach((appointment) => scheduler.scheduleAppointment(appointment));
+    surgeries.forEach((surgery) => scheduler.scheduleSurgery(surgery));
+
+    // Assign tasks
+    tasks.forEach((task) => scheduler.assignTask(task));
+
+    // Set doctor availability and operation theatre availability
+    doctors.forEach((doctor) =>
+      scheduler.setDoctorAvailability(doctor.id, doctor.workingHours, doctor.availableDays)
+    );
+    operationTheatres.forEach((theatre) =>
+      scheduler.setOperationTheatreAvailability(theatre.id, theatre.availableSlots)
+    );
+
+    // Display scheduled appointments
+    scheduler.displayAppointments();
+
+    // Additional operations as needed
+  } catch (error) {
+    console.error('Error initializing data:', error);
+  }
+}
+
+// Initialize data and perform scheduling operations
+initializeData().then(() => {
+  // You can add more operations here or start your application logic
 });
-scheduler.registerDoctor({
-  id: 2,
-  name: 'Dr. Ranveer',
-  workingHours: { start: '08:00', end: '16:00' },
-  availableDays: ['Tuesday', 'Thursday'],
-});
-
-// Register patients
-scheduler.registerPatient({ id: 1, name: 'Karan' });
-scheduler.registerPatient({ id: 2, name: 'Karishma' });
-
-// Set doctor availability
-scheduler.setDoctorAvailability(1, { start: '09:00', end: '17:00' }, ['Monday', 'Wednesday']);
-scheduler.setDoctorAvailability(2, { start: '08:00', end: '16:00' }, ['Tuesday', 'Thursday']);
-
-// Schedule appointment
-scheduler.scheduleAppointment({
-  id: 1,
-  patientId: 1,
-  doctorId: 1,
-  dateTime: '2023-08-15 10:00',
-});
-
-// Display scheduled appointments
-scheduler.displayAppointments();
-
